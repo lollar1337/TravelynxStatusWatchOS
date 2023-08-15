32e8a2ee49a31b3d5f68bdcdab0da760da20547f
Lola <mail@1lola.me>
2023-08-15 23:16:54 +0200

    initial commit

---
+++ TravelynxStatus Watch App/ContentView.swift
@@ -0,0 +1,132 @@
+//
+//  ContentView.swift
+//  TravelynxStatus Watch App
+//
+//  Created by Lola Schwan on 15.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var TrainType: String = "Train Type"
+    @State private var Destination: String = "Destination"
+    @State private var Status: String = "Status"
+    
+    func fetchDataFromServer() {
+        if let url = URL(string: "https://travelynx.de/api/v1/status/1007-397766d5-2e36-484b-8c60-e70be257c47a") {
+            let session = URLSession.shared
+            let task = session.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+                
+                if let data = data {
+                                if let jsonString = String(data: data, encoding: .utf8) {
+                                    //print("Raw JSON: \(jsonString)") // Print the raw JSON data
+                                    
+                                    do {
+                                        let decoder = JSONDecoder()
+                                        let checkIn = try decoder.decode(CheckIn.self, from: data)
+                                        //print("Decoded CheckIn: \(checkIn)")
+                                        if (checkIn.train.line == nil) {
+                                            self.TrainType = checkIn.train.type + " " + checkIn.train.no
+                                        } else {
+                                            self.TrainType = checkIn.train.type + (checkIn.train.line != nil ? " " + checkIn.train.line! : "")
+                                        }
+                                        self.Destination = "nach "+checkIn.toStation.name
+                                        if (checkIn.checkedIn == true) {
+                                            self.Status = "eingecheckt"
+                                        } else {
+                                            self.Status = "nicht eingecheckt"
+                                        }
+                                    } catch {
+                                        print("Error decoding data: \(error)")
+                                    }
+                                }
+                            }
+            }
+            
+            print("Starting network request...")
+            task.resume()
+        }
+    }
+
+    
+    var body: some View {
+       
+        HStack(spacing:10){
+            Image(systemName: "tram.fill")
+                .font(.system(size: 13))
+                .foregroundColor(.indigo)
+                .offset(y: -26)
+            Text("Travelynx Status")
+                .font(.system(size: 18))
+                .fontWeight(.bold)
+                .offset(y: -26)
+            
+        }
+        VStack(alignment: .leading) {
+            Text(TrainType)
+                .font(.system(size: 20))
+                .fontWeight(.bold)
+                .offset(x:10)
+            Text(Destination)
+                .font(.system(size: 12))
+                .fontWeight(.regular)
+                .offset(x:10)
+            Text(Status)
+                .font(.system(size: 12))
+                .fontWeight(.regular)
+                .offset(x:10 ,y:10)
+            Button("refresh") { fetchDataFromServer() }
+                .offset(x: 0, y: 35)
+        }
+        .onAppear {
+            fetchDataFromServer()
+        }
+    }
+}
+
+
+struct CheckIn: Decodable {
+    
+    struct Location: Codable {
+        let ds100: String
+        let latitude: Double
+        let longitude: Double
+        let name: String
+        let realTime: Int
+        let scheduledTime: Int
+        let uic: Int
+    }
+    
+    struct Train: Codable {
+        let id: String
+        let line: String?
+        let no: String
+        let type: String
+    }
+    
+    struct Visibility: Codable {
+        let desc: String
+        let level: Int
+    }
+    
+    let actionTime: Int
+    let checkedIn: Bool
+    let comment: String?
+    let deprecated: Bool
+    let fromStation: Location
+    //let intermediateStops: Array // You can replace Any with the appropriate type
+    let toStation: Location
+    let train: Train
+    let visibility: Visibility
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TravelynxStatus Watch App/TravelynxStatusApp.swift
@@ -0,0 +1,18 @@
+//
+//  TravelynxStatusApp.swift
+//  TravelynxStatus Watch App
+//
+//  Created by Lola Schwan on 15.08.23.
+//
+
+import SwiftUI
+
+@main
+struct TravelynxStatus_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
